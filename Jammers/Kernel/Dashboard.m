5fcc86470642d2e76d1940d49ab7e73f6c2ef8c3
Bob Sandheinrich <bobs@wolfram.com>
2020-09-07 20:15:12 -0500

    game results on dashboard are better

--- Jammers/Kernel/Dashboard.m
+++ Jammers/Kernel/Dashboard.m
@@ -17,15 +17,16 @@ DashboardNotebook[league_]:=Notebook[{
 	leagueLeaderboard[league],
 	Cell["Game History","Section"],
 	leagueGameHistory[league]
-}]
+},System`ClickToCopyEnabled->False]
 
 leagueTitleCell[league_]:=Cell[league,"Title"]
 
 leagueLeaderboard[league_]:=Cell[
 	BoxData[ToBoxes[Dataset[getJammerData["Players",league]]]],"Output"]
 
-leagueGameHistory[league_]:=Cell[
-	BoxData[ToBoxes[Dataset[getJammerData["Games",league]]]],"Output"]
+leagueGameHistory[league_]:=Cell[CellGroupData[
+	KeyValueMap[gameRow,getJammerData["Games",league]]
+,Open],"Output"]
 	
 linksCell[league_]:=Cell[
 	BoxData@ToBoxes@Column[
@@ -41,6 +42,25 @@ linksCell[league_]:=Cell[
 DeployDashboard[args___]:=deployDashboard[args]
 deployDashboard[league_]:=(DeleteObject[CloudObject[dashboardlocation[league]]];
 	CloudDeploy[DashboardNotebook[league],dashboardlocation[league],Permissions->"Public"])
+
+
+gameRow[game_,gameData_]:=
+	Cell[BoxData[ToBoxes[
+		Row[{
+			Style["Game ",FontFamily->"Source Sans Pro"],
+			game,": ",
+			Style[StringRiffle[Values[gameData["t1"]]," & "],FontFamily->"Source Sans Pro"]," ",
+			Style[gameData["Score"]["t1"],Bold,16],
+			" - ",
+			Style[StringRiffle[Values[gameData["t2"]]," & "], FontFamily->"Source Sans Pro"]," ",
+			Style[gameData["Score"]["t2"],Bold,16],"   ",
+			Hyperlink["Edit \[RightGuillemet]",URL["forms/editgame?game="<>ToString[game]]]
+			
+		}]
+	]],"Output"]
+	
+	
+
 
 End[]
 
--- Jammers/Kernel/Deployment.m
+++ Jammers/Kernel/Deployment.m
@@ -4,6 +4,7 @@ BeginPackage["Jammers`"]
 
 Jammers`DeployCode
 Jammers`DeployGameForm
+Jammers`DeployEditGameForm
 Jammers`DeployPlayerForm
 
 Jammers`CreateLeague
@@ -54,6 +55,42 @@ DeployGameForm[league_]:=With[{dumpfile=$dumppath,l=league},
 	]
 ]
 
+
+DeployEditGameForm[league_]:=With[{dumpfile=$dumppath,l=league},
+	CloudDeploy[APIFunction[{"game"->"Integer"},
+		(Clear["Jammers`","Jammers`Private`"];
+		Get[dumpfile];
+		With[{players=playerList[l],g=#game,data=getJammerData["Games",l][#game]},
+		
+		FormFunction[{
+		{"t1","Team 1"}-><|"Interpreter" -> 
+		  CompoundElement[{"player1" -> players, "player2" -> players}], 
+		 "Input" -> data["t1"]
+		 |>,
+		{"t2","Team 2"}-><|"Interpreter" -> 
+		  CompoundElement[{"player1" -> players, "player2" -> players}], 
+		 "Input" -> data["t2"]
+		 |>,
+		"Score"-><|"Interpreter" -> 
+		  CompoundElement[{{"t1","Team 1"} -> "Integer", {"t2","Team 2"} -> "Integer"}], 
+		 "Input" -> data["Score"]
+		 |>
+	},
+		(
+		Clear["Jammers`","Jammers`Private`"];
+		Get[dumpfile];
+		With[{},
+			updateJammerData["Games",l,<|g->#|>];
+			updatePlayersData[l,#];
+		
+		HTTPRedirect[deployDashboard[league]]
+		];
+		)&,
+		AppearanceRules->{"Title"->"Record Game Results"}
+	]])&],
+	URLBuild[{leagueDir[l],"forms","newgame"}],Permissions->"Public"
+	]
+]
 
 
 DeployPlayerForm[league_]:=With[{dumpfile=$dumppath},
